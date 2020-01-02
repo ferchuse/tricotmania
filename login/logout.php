e7026f0f43f6d9532d4e95e723932eb6edf6c454
ferchuse <sistemas@glifo.mx>
2020-01-02 15:17:40 -0600

    Update logout.php

--- login/logout.php
+++ login/logout.php
@@ -1,3 +1,3 @@
 <?php 
-	session_start();	session_destroy();		setcookie("id_usuarios", "",  0, "/");	setcookie("permiso_usuarios", "",  0, "/");	setcookie("nombre_usuarios", "",  0, "/");		header("location:main_login.php");	
+	session_start();	session_destroy();		setcookie("id_usuarios", "",  0, "/");	setcookie("permiso_usuarios", "",  0, "/");	setcookie("nombre_usuarios", "",  0, "/");		header("location:index.php");	
 ?>
