0bf51f492bbef625150d83bf45b585338b5f6edb
ferchuse <sistemas@glifo.mx>
2020-01-11 04:37:50 -0600

    Cierra turno

--- login/logout.php
+++ login/logout.php
@@ -1,3 +1,4 @@
 <?php 
-	session_start();	session_destroy();		setcookie("id_usuarios", "",  0, "/");	setcookie("permiso_usuarios", "",  0, "/");	setcookie("nombre_usuarios", "",  0, "/");		header("location:index.php");	
+	session_start();	session_destroy();		setcookie("id_turnos", "",  0, "/");
+	setcookie("id_usuarios", "",  0, "/");	setcookie("permiso_usuarios", "",  0, "/");	setcookie("nombre_usuarios", "",  0, "/");		header("location:index.php");	
 ?>
