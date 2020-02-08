1d1f0f54a5e48a21e1944e6f0c1140b083a665b6
ferchuse <sistemas@glifo.mx>
2020-02-08 12:18:40 -0600

    Update index.php

--- ventas/index.php
+++ ventas/index.php
@@ -95,192 +95,192 @@
 					<a data-toggle="tab" href="#tab1">
 						<input class="nombre_cliente" value="Cliente 1" >
 						<input type="hidden" class="id_ventas" value="">
-					<span class="badge">0</span>
-				</a>
-			</li>
-			<li hidden>
-				<a data-toggle="tab" href="#tab2">
-					<input  class="nombre_cliente" value="Cliente 2" >
-					<input type="hidden" class="id_ventas" value="">
-					<span class="badge">0</span>
-				</a>
-			</li>
+						<span class="badge">0</span>
+					</a>
+				</li>
+				<li  class="hidden" hidden>
+					<a class="d-none" hidden data-toggle="tab" href="#tab2">
+						<input  class="nombre_cliente" value="Cliente 2" >
+						<input type="hidden" class="id_ventas" value="">
+						<span class="badge">0</span>
+					</a>
+				</li>
+				
+				<li >
+					<a class="hidden" hidden data-toggle="tab" href="#tab3">
+						<input  class="nombre_cliente" value="Cliente 3" >
+						<input type="hidden" class="id_ventas" value="">
+						<span class="badge">0</span>
+					</a>
+				</li>
+				
+			</ul>
 			
-			<li hidden>
-				<a data-toggle="tab" href="#tab3">
-					<input  class="nombre_cliente" value="Cliente 3" >
-					<input type="hidden" class="id_ventas" value="">
-					<span class="badge">0</span>
-				</a>
-			</li>
-			
-		</ul>
-		
-		<div class="tab-content">
-			<div id="tab1" class="tab-pane in active">
-				<div class="productos">
-					<table id="tabla_venta" class="tabla_venta table table-bordered table-condensed">
-						<thead class="bg-success">
-							<tr>
-								<th class="text-center">Cantidad</th>
-								<th class="text-center">Unidad</th>
-								<th class="text-center">Descripcion del Producto</th>
-								<th class="text-center">Precio Unitario</th>
-								<th class="text-center">Importe</th>
-								<th class="text-center">% Descuento</th>
-								<th class="text-center">Descuento</th>
-								<th class="text-center">Existencia</th>
-								<th class="text-center">Acciones</th>
-							</tr>
-						</thead>
-						<tbody>
-							
-						</tbody>
-					</table>
-				</div>
-				<section id="footer">
-					<div class="row">
-						<div class="col-sm-1 lead">
-							<label>Artículos </label>
-							<input class="form-control articulos" type="number" autocomplete="off" readonly value="0">
-						</div>
-						
-						<div class="col-sm-9 col-6  text-right ">
-							<label class="venta lbl_totales"  for="">Subtotal:</label>  <br>
-							<label class="venta lbl_totales" for="">Descuento:</label>  <br>
-							<label class="venta lbl_totales" for="">Total:</label> 
-						</div>
-						<div class="col-sm-2 col-6  venta">
-							<input readonly type="text" class="form-control text-right venta subtotal" value="0" >
-							<input readonly type="text" class="form-control text-right venta total_descuento" value="0">
-							<input readonly  type="text" class="form-control text-right venta total" value="0" >
-						</div>
+			<div class="tab-content">
+				<div id="tab1" class="tab-pane in active">
+					<div class="productos">
+						<table id="tabla_venta" class="tabla_venta table table-bordered table-condensed">
+							<thead class="bg-success">
+								<tr>
+									<th class="text-center">Cantidad</th>
+									<th class="text-center">Unidad</th>
+									<th class="text-center">Descripcion del Producto</th>
+									<th class="text-center">Precio Unitario</th>
+									<th class="text-center">Importe</th>
+									<th class="text-center">% Descuento</th>
+									<th class="text-center">Descuento</th>
+									<th class="text-center">Existencia</th>
+									<th class="text-center">Acciones</th>
+								</tr>
+							</thead>
+							<tbody>
+								
+							</tbody>
+						</table>
 					</div>
-				</section>
+					<section id="footer">
+						<div class="row">
+							<div class="col-sm-1 lead">
+								<label>Artículos </label>
+								<input class="form-control articulos" type="number" autocomplete="off" readonly value="0">
+							</div>
+							
+							<div class="col-sm-9 col-6  text-right ">
+								<label class="venta lbl_totales"  for="">Subtotal:</label>  <br>
+								<label class="venta lbl_totales" for="">Descuento:</label>  <br>
+								<label class="venta lbl_totales" for="">Total:</label> 
+							</div>
+							<div class="col-sm-2 col-6  venta">
+								<input readonly type="text" class="form-control text-right venta subtotal" value="0" >
+								<input readonly type="text" class="form-control text-right venta total_descuento" value="0">
+								<input readonly  type="text" class="form-control text-right venta total" value="0" >
+							</div>
+						</div>
+					</section>
+				</div>
+				
+				<div id="tab2" class="tab-pane">
+					<div class="productos">
+						<table class="tabla_venta table table-bordered table-condensed">
+							<thead class="bg-success">
+								<tr>
+									<th class="text-center">Cantidad</th>
+									<th class="text-center">Unidad</th>
+									<th class="text-center">Descripcion del Producto</th>
+									<th class="text-center">Precio Unitario</th>
+									<th class="text-center">Importe</th>
+									<th class="text-center">% Descuento</th>
+									<th class="text-center">Descuento</th>
+									<th class="text-center">Existencia</th>
+									<th class="text-center">Acciones</th>
+								</tr>
+							</thead>
+							<tbody>
+								
+							</tbody>
+						</table>
+					</div>
+					<section id="footer">
+						<div class="row">
+							<div class="col-sm-1 lead">
+								<label>Artículos </label>
+								<input class="form-control articulos" type="number" autocomplete="off" readonly value="0">
+							</div>
+							
+							
+							<div class="col-sm-9 col-6  text-right ">
+								<label class="venta lbl_totales"  for="">Subtotal:</label>  <br>
+								<label class="venta lbl_totales" for="">Descuento:</label>  <br>
+								<label class="venta lbl_totales" for="">Total:</label> 
+							</div>
+							<div class="col-sm-2 col-6  venta">
+								<input readonly type="text" class="form-control text-right venta subtotal" value="0" >
+								<input readonly type="text" class="form-control text-right venta total_descuento" value="0">
+								<input readonly  type="text" class="form-control text-right venta total" value="0" >
+							</div>
+						</div>
+					</section>
+				</div>
+				<div id="tab3" class="tab-pane">
+					<div class="productos">
+						<table class="tabla_venta table table-bordered table-condensed">
+							<thead class="bg-success">
+								<tr>
+									<th class="text-center">Cantidad</th>
+									<th class="text-center">Unidad</th>
+									<th class="text-center">Descripcion del Producto</th>
+									<th class="text-center">Precio Unitario</th>
+									<th class="text-center">Importe</th>
+									<th class="text-center">% Descuento</th>
+									<th class="text-center">Descuento</th>
+									<th class="text-center">Existencia</th>
+									<th class="text-center">Acciones</th>
+								</tr>
+							</thead>
+							<tbody>
+								
+							</tbody>
+						</table>
+					</div>
+					<section id="footer">
+						<div class="row">
+							<div class="col-sm-1 lead">
+								<label>Artículos </label>
+								<input class="form-control articulos" type="number" autocomplete="off" readonly value="0">
+							</div>
+							
+							<div class="col-sm-9 col-6  text-right ">
+								<label class="venta lbl_totales"  for="">Subtotal:</label>  <br>
+								<label class="venta lbl_totales" for="">Descuento:</label>  <br>
+								<label class="venta lbl_totales" for="">Total:</label> 
+							</div>
+							<div class="col-sm-2 col-6  venta">
+								<input readonly type="text" class="form-control text-right venta subtotal" value="0" >
+								<input readonly type="text" class="form-control text-right venta total_descuento" value="0">
+								<input readonly  type="text" class="form-control text-right venta total" value="0" >
+							</div>
+						</div>
+					</section>
+				</div>
 			</div>
 			
-			<div id="tab2" class="tab-pane">
-				<div class="productos">
-					<table class="tabla_venta table table-bordered table-condensed">
-						<thead class="bg-success">
-							<tr>
-								<th class="text-center">Cantidad</th>
-								<th class="text-center">Unidad</th>
-								<th class="text-center">Descripcion del Producto</th>
-								<th class="text-center">Precio Unitario</th>
-								<th class="text-center">Importe</th>
-								<th class="text-center">% Descuento</th>
-								<th class="text-center">Descuento</th>
-								<th class="text-center">Existencia</th>
-								<th class="text-center">Acciones</th>
-							</tr>
-						</thead>
-						<tbody>
-							
-						</tbody>
-					</table>
-				</div>
-				<section id="footer">
-					<div class="row">
-						<div class="col-sm-1 lead">
-							<label>Artículos </label>
-							<input class="form-control articulos" type="number" autocomplete="off" readonly value="0">
-						</div>
-						
-						
-						<div class="col-sm-9 col-6  text-right ">
-							<label class="venta lbl_totales"  for="">Subtotal:</label>  <br>
-							<label class="venta lbl_totales" for="">Descuento:</label>  <br>
-							<label class="venta lbl_totales" for="">Total:</label> 
-						</div>
-						<div class="col-sm-2 col-6  venta">
-							<input readonly type="text" class="form-control text-right venta subtotal" value="0" >
-							<input readonly type="text" class="form-control text-right venta total_descuento" value="0">
-							<input readonly  type="text" class="form-control text-right venta total" value="0" >
-						</div>
-					</div>
-				</section>
-			</div>
-			<div id="tab3" class="tab-pane">
-				<div class="productos">
-					<table class="tabla_venta table table-bordered table-condensed">
-						<thead class="bg-success">
-							<tr>
-								<th class="text-center">Cantidad</th>
-								<th class="text-center">Unidad</th>
-								<th class="text-center">Descripcion del Producto</th>
-								<th class="text-center">Precio Unitario</th>
-								<th class="text-center">Importe</th>
-								<th class="text-center">% Descuento</th>
-								<th class="text-center">Descuento</th>
-								<th class="text-center">Existencia</th>
-								<th class="text-center">Acciones</th>
-							</tr>
-						</thead>
-						<tbody>
-							
-						</tbody>
-					</table>
-				</div>
-				<section id="footer">
-					<div class="row">
-						<div class="col-sm-1 lead">
-							<label>Artículos </label>
-							<input class="form-control articulos" type="number" autocomplete="off" readonly value="0">
-						</div>
-						
-						<div class="col-sm-9 col-6  text-right ">
-							<label class="venta lbl_totales"  for="">Subtotal:</label>  <br>
-							<label class="venta lbl_totales" for="">Descuento:</label>  <br>
-							<label class="venta lbl_totales" for="">Total:</label> 
-						</div>
-						<div class="col-sm-2 col-6  venta">
-							<input readonly type="text" class="form-control text-right venta subtotal" value="0" >
-							<input readonly type="text" class="form-control text-right venta total_descuento" value="0">
-							<input readonly  type="text" class="form-control text-right venta total" value="0" >
-						</div>
-					</div>
-				</section>
+			<div class="sticky-footer">
+				
+				<button class="btn btn-warning btn-lg" FORM="" id="btn_pendiente">
+					F6 - Pendiente
+				</button>
+				
+				<button class="btn btn-success btn-lg" FORM="" id="cerrar_venta">
+					F12 - Cobrar
+				</button>
+				
 			</div>
 		</div>
 		
-		<div class="sticky-footer">
-			
-			<button class="btn btn-warning btn-lg" FORM="" id="btn_pendiente">
-				F6 - Pendiente
-			</button>
-			
-			<button class="btn btn-success btn-lg" FORM="" id="cerrar_venta">
-				F12 - Cobrar
-			</button>
+		<div id="ticket" class="visible-print">
 			
 		</div>
-	</div>
-	
-	<div id="ticket" class="visible-print">
 		
-	</div>
+		<?php include('forma_pago.php'); ?>
+		<?php include('../scripts_carpetas.php'); ?>
+		
+		<script>
+			$.getScript('https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js', function() {
+				$('.cliente')
+				.SpatialNavigation()
+				.focus(function() {
+					$(this).addClass("bg-info");
+				})
+				.blur(function() {
+					$(this).removeClass('bg-info');
+				})
+				.first()
+				.focus();
+			});
+		</script>
+		
+		<script src="ventas.js?v=<?= date("d-m-Y-H-i-s")?>"></script>
+		
+	</body>
 	
-	<?php include('forma_pago.php'); ?>
-	<?php include('../scripts_carpetas.php'); ?>
-	
-	<script>
-		$.getScript('https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js', function() {
-			$('.cliente')
-			.SpatialNavigation()
-			.focus(function() {
-				$(this).addClass("bg-info");
-			})
-			.blur(function() {
-				$(this).removeClass('bg-info');
-			})
-			.first()
-			.focus();
-		});
-	</script>
-	
-	<script src="ventas.js?v=<?= date("d-m-Y-H-i-s")?>"></script>
-	
-</body>
-
 </html>
